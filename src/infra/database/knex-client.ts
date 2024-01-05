89fe615c1075ba8a7e7b0ae04734bd39a75ce60b
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-01-05 17:06:52 -0300

    feat: moves database path to `.env` file

--- src/infra/database/knex-client.ts
+++ src/infra/database/knex-client.ts
@@ -1,10 +1,16 @@
+import 'dotenv/config'
+
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env variable not found')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   useNullAsDefault: true,
   connection: {
-    filename: './src/infra/database/app.db',
+    filename: process.env.DATABASE_URL,
   },
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './infra/database'
+import { randomUUID } from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Test transaction',
+      amount: 100,
+    })
+    .returning('*')
+  return transaction
 })
 
 app
